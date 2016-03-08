fcfa54a3ff9ded0f233ce19b75bce8ea17542fca
ansteh <stehle.andre@googlemail.com>
2016-03-08 22:21:32 +0100

    got wrap + es6 enhancement

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const request = require('./lib/request.js');
-const query   = require('./lib/query.js');
-
-const datamuse = {};
-
-Object.keys(query).forEach(function(name){
-  datamuse[name] = (args) => {
-    return request(query[name](args));
-  };
-});
-
-module.exports = datamuse;
+module.exports = require('./lib/query.js');
